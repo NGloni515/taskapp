fd202bdb6d1701ee99e1cb73c1645eddff2ecb10
NGloni515 <angelonischolar@gmail.com>
2020-08-15 14:27:49 -0500

    holiwis

--- src/emails/account.js
+++ src/emails/account.js
@@ -5,23 +5,22 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 const sendWelcomeEmail = (email, name) => {
     sgMail.send({
         to: email,
-        from: 'angelonischolar@gmail.com',
-        subject: 'Haz sido hackeado con éxito',
-        text: `Bienvenido a Hobson, ${name}. Gracias por unirte`,
-        html: ''
+        from: 'andrew@mead.io',
+        subject: 'Thanks for joining in!',
+        text: `Welcome to the app, ${name}. Let me know how you get along with the app.`
     })
 }
 
 const sendCancelationEmail = (email, name) => {
     sgMail.send({
         to: email,
-        from: 'angelonischolar@gmail.com',
-        subject: 'Lo sentimos!!!',
-        text: `Hasta luego, ${name}, esperamos tenerte de vuelta pronto`
+        from: 'andrew@mead.io',
+        subject: 'Sorry to see you go!',
+        text: `Goodbye, ${name}. I hope to see you back sometime soon.`
     })
 }
 
 module.exports = {
-    sendWelcomeEmail, 
+    sendWelcomeEmail,
     sendCancelationEmail
 }
